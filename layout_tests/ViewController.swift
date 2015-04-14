869556eac8154171ce1629ee0351b5c58e562355
Steven Cassidy <steven@mumsnet.com>
2015-04-13 22:15:34 -0400

    Add a test post something to server async

--- layout_tests/ViewController.swift
+++ layout_tests/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(sender : AnyObject) {
         println("I got tapped")
         println(textField.text)
+        let url = NSURL(string: "http://localhost:4567/weight")
+        var request:NSMutableURLRequest = NSMutableURLRequest(URL:url!)
+        request.HTTPMethod = "POST"
+        var bodyData = "wgt=\(textField.text)"
+        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
+            (response, data, error) in
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        }
 
     }
     @IBAction func viewTapped(sender : AnyObject) {
