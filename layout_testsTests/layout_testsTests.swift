4c21252a524bdcc372805ef01c585fd4f46f31b7
Steven Cassidy <steven@mumsnet.com>
2015-04-13 21:36:06 -0400

    Initial Commit

---
+++ layout_testsTests/layout_testsTests.swift
@@ -0,0 +1,36 @@
+//
+//  layout_testsTests.swift
+//  layout_testsTests
+//
+//  Created by Steven Cassidy on 4/13/15.
+//  Copyright (c) 2015 Steven Cassidy. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class layout_testsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
