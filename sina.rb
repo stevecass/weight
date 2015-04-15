fe39d9c404a1ea3e786690f50d3b7117845e470b
stevecass <steven@mumsnet.com>
2015-04-15 12:41:10 -0400

    Update toy sinatra server to send history

--- sina.rb
+++ sina.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
+require 'json'
+
+sample = {
+  '2015-04-01' => 198, '2015-04-02' => 197.6, '2015-04-03' => 197,
+  '2015-04-04' => 198, '2015-04-05' => 196.6, '2015-04-06' => 196,
+  '2015-04-07' => 195.8, '2015-04-08' => 196.2, '2015-04-09' => 195.2
+  }
+
 
 get '/' do
   [200, 'Hello']
+end
+
+get '/weight' do
+  content_type :json
+  sample.to_json
 end
 
 post '/weight' do
