2687f0ce4af441dc291a4ec569ad27a847f99a17
stevecass <steven@mumsnet.com>
2015-04-15 11:07:05 -0400

    Update to WeighIn struct

--- layout_tests/WeighIn.swift
+++ layout_tests/WeighIn.swift
@@ -5,5 +5,15 @@
 //  Created by Steven Cassidy on 4/15/15.
 //  Copyright (c) 2015 Steven Cassidy. All rights reserved.
 //
-
 import Foundation
+
+struct WeighIn {
+    var date: NSDate
+    var weight: Double
+    init(dstr:String, w:String) {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        date = formatter.dateFromString(dstr)!
+        weight = (w as NSString).doubleValue
+    }
+}
