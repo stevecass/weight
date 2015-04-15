22921e59e93fd5a443fdb47ab676cb6fe4c0eb80
stevecass <steven@mumsnet.com>
2015-04-15 10:02:37 -0400

    Send full date from date picker

--- layout_tests/ViewController.swift
+++ layout_tests/ViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var textField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+
+    /* Feel like there must be a shorter / built-in way to do this */
+    func getIsoDateStringFromPicker() -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z"
+        formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        return formatter.stringFromDate(datePicker.date)
+    }
 
     @IBAction func buttonTapped(sender : AnyObject) {
         println("I got tapped")
@@ -18,7 +28,9 @@ class ViewController: UIViewController {
         let url = NSURL(string: "http://localhost:4567/weight")
         var request:NSMutableURLRequest = NSMutableURLRequest(URL:url!)
         request.HTTPMethod = "POST"
+        let dateString = datePicker.date
         var bodyData = "wgt=\(textField.text)"
+        bodyData += ("&dt=" + getIsoDateStringFromPicker())
         request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
             (response, data, error) in
